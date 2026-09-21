7b0ae2cffb921fc44d5971bec003266f607cdeb3
William D. Goins V ("Quin") <116991920+wdg0008@users.noreply.github.com>
2026-09-20 20:52:09 -0500

    Updates to loop plots over the data

--- plotCities.m
+++ plotCities.m
@@ -6,38 +6,59 @@ clearvars;
 close all;
 clc;
 
-csvdata = readtable("time_serial.csv", "CommentStyle", "#");
+cases = ["Serial", "PTH1", "PTH2"]'; % column-major in memory
+serialData = readtable("time_serial.csv", "CommentStyle", "#");
+PTH1Data = readtable("time_PTH1.csv", "CommentStyle", "#");
+PTH2Data = readtable("time_PTH2.csv", "CommentStyle", "#");
 
-n = csvdata.Cities;
-T = csvdata.Time_s;
+%% Analyze serial case
 
-% Ignore measurements below 1 second
-idx = T >= 1;
+n = serialData.Cities;
+t = serialData.Time_s;
 
-n_fit = n(idx);
-T_fit = T(idx);
+N = {serialData.Cities, PTH1Data.Cities, PTH2Data.Cities};
+T = {serialData.Time_s, PTH1Data.Time_s, PTH2Data.Time_s};
 
-% Fit linearized log(T) = log(C) + log(n!)
-% (polyfit(x,y,n) n-degree polynomial
-p = polyfit(gammaln(n_fit + 1), log(T_fit), 1);
-
-slope = p(1);
-C = exp(p(2)); % intercept is log(C)
-
-fprintf("T(n) = %.4e * n! seconds\n", C);
-fprintf("Linear regression slope is %g.\n", p(1));
-disp("A slope near 1 means this is a good fit.")
+C = zeros(3,1); % estimate of constant multiple of O(n!)
+s = zeros(3,1); % estimate of slope of ln, which would be a linear power
 
 figure;
-plot(n, T);
-xlabel("Number of Cities");
-ylabel("Execution Time (s)");
-title("Serial TSP Execution Time");
-grid on;
+for i = 1:3
+    [C(i), s(i)] = fitFactorial(N{i}, T{i});
 
-figure;
-plot(n, log10(T));
-xlabel("Number of Cities");
-ylabel(" log_{10}(T)");
-title("Serial TSP Execution Time");
-grid on;
+    fprintf("\nBeginning of %s case\n\n", cases(i));
+    fprintf("T(n) = %.4e * n! seconds\n", C(i));
+    fprintf("Linear regression slope is %g.\n", s(i));
+    disp("A slope near 1 means this is a good fit.")
+
+    subplot(3,2,(2*i-1))
+    plot(N{i}, T{i});
+    xlabel("Number of Cities");
+    ylabel("Execution Time (s)");
+    title(sprintf("%s TSP Execution Time", cases(i)));
+    grid on;
+
+    subplot(3,2,(2*i))
+    plot(N{i}, log10(T{i}));
+    xlabel("Number of Cities");
+    ylabel(" log_{10}(T)");
+    title(sprintf("%s TSP Execution Time", cases(i)));
+    grid on;
+end
+
+function [C, slope] = fitFactorial(n, t)
+    assert(length(n) == length(t));
+
+    threshold_s = 1; % ignore times less than this
+    idx = (t >= threshold_s);
+
+    n_fit = n(idx);
+    T_fit = t(idx);
+
+    % Fit linearized log(T) = log(C) + log(n!)
+    % (polyfit(x,y,n) n-degree polynomial
+    p = polyfit(gammaln(n_fit + 1), log(T_fit), 1);
+
+    slope = p(1);
+    C = exp(p(2)); % intercept is log(C)
+end
